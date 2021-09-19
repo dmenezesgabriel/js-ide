b07cc549e867fa140784a37ca012212f6ca7b119
Gabriel Dias Menezes <dmenezes.gabriel@gmail.com>
2021-09-19 17:35:33 -0300

    Clean editor controller methods

--- assets/js/controllers/editor.js
+++ assets/js/controllers/editor.js
@@ -8,10 +8,18 @@ export class EditorController {
     let $ = document.querySelector.bind(document);
     this._content = "";
     this._currentEditorId = 0;
+    this._currentEditor;
     // controller
     this._editorList = new EditorList();
     this._editorView = new EditorView($("#editorView"));
     this._tabView = new TabView($("#tabView"));
+
+    $("#save-current-editor-content").onclick = this._saveEditorText;
+    $("#add-new-editor").onclick = this._addEditor.bind(this);
+    $("#execute-btn").onclick = this._runCode.bind(this);
+    $("#clear-btn").onclick = function () {
+      console.clear();
+    };
   }
 
   _createEditor() {
@@ -23,35 +31,56 @@ export class EditorController {
     );
   }
 
-  initEditor() {
+  _addEditor() {
+    // if editor exists, store editor content before change tabs
+    if (typeof this._currentEditor !== "undefined") {
+      this._currentEditor.content = this._getEditorText();
+    }
+    let editor = this._createEditor();
+    this._editorList.add(editor);
+    this._editorView.update(this._editorList);
+    this._tabView.update(this._editorList);
+    this._initEditor();
+    this._currentEditor = editor;
+  }
+
+  _initEditor() {
     this._editorElement = document.querySelector(
       `#editor-${this._currentEditorId}`
     );
     this._editorElement.textContent = `function echo(m) {\n\treturn m;\n}\nconsole.log(echo("Hello World ${this._currentEditorId}"));`;
     this._editorElement.style.fontSize = "15px";
 
-    this._currentEditor = ace.edit(`editor-${this._currentEditorId}`);
-    this._currentEditor.session.setMode("ace/mode/javascript");
-    this._currentEditor.setTheme("ace/theme/dracula");
-    this._currentEditor.session.setTabSize(4);
-    this._currentEditor.session.setUseWrapMode(true);
-    this._editorElement.style.display = "block";
+    this._ace = ace.edit(`editor-${this._currentEditorId}`);
+    this._ace.session.setMode("ace/mode/javascript");
+    this._ace.setTheme("ace/theme/dracula");
+    this._ace.session.setTabSize(4);
+    this._ace.session.setUseWrapMode(true);
     this._editorElement.style.display = "block";
     document.querySelector(`#tab-editor-${this._currentEditorId}`).className +=
       " active-tab";
   }
 
-  addEditor() {
-    let editor = this._createEditor();
-    this._editorList.add(editor);
-    this._editorView.update(this._editorList);
-    this._tabView.update(this._editorList);
-    this.initEditor();
+  _getEditorText() {
+    return this._ace.getValue();
+  }
+
+  _saveEditorText() {
+    let textContent = editorController._getEditorText();
+    var downloadableLink = document.querySelector(
+      "#save-current-editor-content"
+    );
+    downloadableLink.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," + encodeURIComponent(textContent)
+    );
+    downloadableLink.download = "script" + ".js";
+    downloadableLink.target = "_blank";
   }
 
   kbd(event) {
     if (event.key === "i") console.clear();
-    if (event.key === "Enter") eval(this._currentEditor.getValue());
+    if (event.key === "Enter") eval(this._ace.getValue());
   }
 
   setKeyShortcuts() {
@@ -69,47 +98,15 @@ export class EditorController {
     });
   }
 
-  setButtonActions() {
-    document
-      .querySelector("#execute-btn")
-      .addEventListener("click", this.runCode.bind(this));
-    document.querySelector("#clear-btn").addEventListener("click", function () {
-      console.clear();
-    });
-  }
-
-  runCode() {
-    eval(this._currentEditor.getValue());
+  _runCode() {
+    eval(this._ace.getValue());
   }
 
   init() {
     this.setKeyShortcuts();
-    this.setButtonActions();
-    this.addEditor();
-  }
-
-  getEditorText() {
-    return this._currentEditor.getValue();
+    this._addEditor();
   }
 }
 
 let editorController = new EditorController();
 editorController.init();
-
-document.querySelector("#save-file").onclick = saveEditorText;
-document.querySelector("#new-editor").onclick = newEditor;
-
-function saveEditorText() {
-  let textContent = editorController.getEditorText();
-  var downloadableLink = document.querySelector("#save-file");
-  downloadableLink.setAttribute(
-    "href",
-    "data:text/plain;charset=utf-8," + encodeURIComponent(textContent)
-  );
-  downloadableLink.download = "script" + ".js";
-  downloadableLink.target = "_blank";
-}
-
-function newEditor() {
-  editorController.addEditor();
-}
